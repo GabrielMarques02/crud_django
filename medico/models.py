90e26feaaced1c65e7e01f49d22c41789184b8fa
GabrielMarques02 <aluno103.23187257gabriel@gmail.com>
2024-05-02 21:26:53 -0300

    completando model especialidade

--- medico/models.py
+++ medico/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 
-# Create your models here.
+
+class Especialidade(models.Model):
+    especialidade = models.CharField(verbose_name='Especialidade', max_length=200)
+    descricao = models.CharField(verbose_name='Descrição', max_length=1000)
+
+    def __str__(self):
+        return self.especialidade
+
+class Medico(models.Model):
+    nome = models.CharField(verbose_name='Nome', max_length=200)
+    endereco = models.CharField(verbose_name='Endereço', max_length=200)
+    email = models.EmailField(verbose_name='Email',)
+    crm = models.CharField(verbose_name='CRM', max_length=11)
+    telefone = models.CharField(verbose_name='Telefone', validators=[])
+    especialidade = ForeignKey(Especialidade, on_delete= models.CASCADE, blank=False, related_name='medico')
+    
+    def __str__(self):
+        return self.nome
+
